a153c260fdbc9eb750b5ed2fb712ee596ff85790
HieuDRG <phanhieu18032011@gmail.com>
2025-10-25 07:51:55 +0700

    Refactor executor check and improve UI layout
    Removed executor check and related logic, updated UI elements, and modified tab structure for better organization.

--- skid.lua
+++ skid.lua
@@ -1,407 +1,85 @@
--- Check Executor 
-local executor = getexecutorname() or identifyexecutor()
-
-if executor then
-    local supportedExecutors = {
-        "Velocity",
-        "Wave",
-        "Arceus",
-        "Swift",
-        "AWP",
-        "Macsploit",
-        "Delta",
-        "Fluxus",
-        "CodeX",
-        "Krnl",
-        "Trigon",
-        "Evon",
-        "Cryptic",
-        "SynapeX",
-        "Potassium",
-        "Argon",
-        "Xeno",
-        "Nezur",
-        "Revelix",
-        "Cubix",
-        "Solara",
-        "Dynamic",
-        "Nebula",
-        "Ronix",
-        "Atlantis",
-        "JJsploit",
-        "Frostware" -- thêm vào thì tùy ae
-    }
-
-    local isExecutorSupported = false
-    for _, name in ipairs(supportedExecutors) do
-        if string.find(executor, name) then
-            isExecutorSupported = true
-            break
-        end
-    end
-
-    if isExecutorSupported then
-        print("Supported Executor!")
-    else
-        game.Players.LocalPlayer:Kick("Not supported TNG Skid executor!") -- kick nếu exec ko support
-    end
-end
-
-repeat
-    wait()
-until game:IsLoaded() and game.Players.LocalPlayer
-
-print("Whitelist success!")
-wait(.5)
-game.StarterGui:SetCore("SendNotification", {
-     Title = "HieuDRG Hub";
-     Text = "Loading";
-     Icon = "";
-     Duration = "2";
-})
-
-wait(.1)
-
-
-----------------------------------------ToogleUi----------------------------------------
-
 local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local ImageLabel = Instance.new("ImageLabel")
+local ImageButton = Instance.new("ImageButton")
 local UICorner = Instance.new("UICorner")
-local TextButton = Instance.new("TextButton")
 
-ScreenGui.Parent = game:GetService("CoreGui")  
+ScreenGui.Parent = game.CoreGui
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-Frame.Parent = ScreenGui
-Frame.AnchorPoint = Vector2.new(0.1, 0.1)
-Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Frame.BackgroundTransparency = 0
-Frame.BorderColor3 = Color3.fromRGB(27, 42, 53)
-Frame.BorderSizePixel = 1
-Frame.Position = UDim2.new(0, 20, 0.1, -6)  
-Frame.Size = UDim2.new(0, 50, 0, 50)
-Frame.Name = "dut dit"
+ImageButton.Parent = ScreenGui
+ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BorderSizePixel = 0
+ImageButton.Position = UDim2.new(0.10615778, 0, 0.16217947, 0)
+ImageButton.Size = UDim2.new(0, 40, 0, 40)
+ImageButton.Draggable = true
+ImageButton.Image = "http://www.roblox.com/asset/?id=83190276951914"
 
-ImageLabel.Parent = Frame
-ImageLabel.Name = "Banana Test"
-ImageLabel.AnchorPoint = Vector2.new(0.5, 0.5)
-ImageLabel.Position = UDim2.new(0.5, 0, 0.5, 0)
-ImageLabel.Size = UDim2.new(0, 40, 0, 40)
-ImageLabel.BackgroundColor3 = Color3.fromRGB(163, 162, 165)
-ImageLabel.BackgroundTransparency = 1
-ImageLabel.BorderSizePixel = 1
-ImageLabel.BorderColor3 = Color3.fromRGB(27, 42, 53)
-ImageLabel.ImageColor3 = Color3.fromRGB(255, 255, 255)
-ImageLabel.Image = "http://www.roblox.com/asset/?id=94439769761633" -- Thay 5009915795 Bằng ảnh của anh em
+UICorner.CornerRadius = UDim.new(1, 10) 
+UICorner.Parent = ImageButton
 
-UICorner.CornerRadius = UDim.new(1, 0)
-UICorner.Parent = Frame
-
-TextButton.Name = "TextButton"
-TextButton.Parent = Frame
-TextButton.AnchorPoint = Vector2.new(0, 0)
-TextButton.Position = UDim2.new(0, 0, 0, 0)
-TextButton.Size = UDim2.new(1, 0, 1, 0)
-TextButton.BackgroundColor3 = Color3.fromRGB(163, 162, 165)
-TextButton.BackgroundTransparency = 1
-TextButton.BorderSizePixel = 1
-TextButton.BorderColor3 = Color3.fromRGB(27, 42, 53)
-TextButton.TextColor3 = Color3.fromRGB(27, 42, 53)
-TextButton.Text = ""
-TextButton.Font = Enum.Font.SourceSans
-TextButton.TextSize = 8
-TextButton.TextTransparency = 0
-
-local TweenService = game:GetService("TweenService")
-local VirtualInputManager = game:GetService("VirtualInputManager")
-
-local zoomedIn = false
-local originalSize = UDim2.new(0, 40, 0, 40)
-local zoomedSize = UDim2.new(0, 30, 0, 30)
-local tweenInfo = TweenInfo.new(0.25, Enum.EasingStyle.Quad, Enum.EasingDirection.Out)
-
-local faded = false
-local fadeInTween = TweenService:Create(Frame, tweenInfo, {BackgroundTransparency = 0.25})
-local fadeOutTween = TweenService:Create(Frame, tweenInfo, {BackgroundTransparency = 0})
-
-TextButton.MouseButton1Down:Connect(function()
-
-    if zoomedIn then
-        TweenService:Create(ImageLabel, tweenInfo, {Size = originalSize}):Play()
-    else
-        TweenService:Create(ImageLabel, tweenInfo, {Size = zoomedSize}):Play()
-    end
-    zoomedIn = not zoomedIn
-
-    if faded then
-        fadeOutTween:Play()
-    else
-        fadeInTween:Play()
-    end
-    faded = not faded
-
-    VirtualInputManager:SendKeyEvent(true, "LeftControl", false, game)
+ImageButton.MouseButton1Down:Connect(function()
+    game:GetService("VirtualInputManager"):SendKeyEvent(true, Enum.KeyCode.End, false, game)
 end)
 
-print("Loading Main Ui")
-wait(.1)
-game.StarterGui:SetCore("SendNotification", {
-     Title = "HieuDRG Hub";
-     Text = "Loading Main Ui";
-     Icon = "";
-     Duration = "2";
-})
-wait(.1)
-
-----------------------------------------Fluent----------------------------------------
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
-local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
-
+repeat wait() until game:IsLoaded()
 local Window = Fluent:CreateWindow({
-    Title = "HieuDRG Hub tổng hợp",
-    SubTitle = "UPDATE V1",
-    TabWidth = 160,
-    Size = UDim2.fromOffset(500, 350),
-    Acrylic = true, 
-    Theme = "Light",
-    MinimizeKey = Enum.KeyCode.LeftControl
+    Title = "TBoy Roblox Tổng Hợp",
+    SubTitle = "Blox Fruit",
+    TabWidth = 157,
+    Size = UDim2.fromOffset(450, 300),
+    Acrylic = true,
+    Theme = "Amethyst",
+    MinimizeKey = Enum.KeyCode.End
 })
-----------------------------------------Tab----------------------------------------
 local Tabs = {
-    Tab1 = Window:AddTab({ Title = "Client", Icon = "" }),
-    Tab2 = Window:AddTab({ Title = "Blox Fruit", Icon = "" }),
-    Tab3 = Window:AddTab({ Title = "Grow a Garden", Icon = "" }),
-    Tab4 = Window:AddTab({ Title = "Steal a Brainrot", Icon = "" }),
-    Tab5 = Window:AddTab({ Title = "99 Night In The Forest ", Icon = "" }),
-    Tab6 = Window:AddTab({ Title = "Hunte Zombie", Icon = "" }),
-    Tab7 = Window:AddTab({ Title = "Settings", Icon = "" }),
-    Tab8 = Window:AddTab({ Title = "Info", Icon = "" }),
+        Main0=Window:AddTab({ Title="Thông Tin" }),
+        Main1=Window:AddTab({ Title="Script Farm" }),
+        Main2=Window:AddTab({ Title="Farm Fruit" }),
+        Main3=Window:AddTab({ Title="Farm Chest" }),
+        Main4=Window:AddTab({ Title="Script Hop" }),
+       Main5=Window:AddTab({ Title="lin tinh" }),
 }
-----------------------------------------clien----------------------------------------
-print("Client Tab Loading")
-wait(1)
-    Tabs.Tab1:AddButton({
-        Title = "SynapseX",
-        Description = "Can Run Script But Use Your Executor's Api",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/LongHip2012/ScriptBloxFruit/refs/heads/main/SynapsexUi.lua"))()
-        end
-    })
-    
-    Tabs.Tab1:AddButton({
-        Title = "ArceusX",
-        Description = "Can Run Script But Use Your Executor's Api",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/chillz-workshop/main/Arceus%20X%20V3"))()
-        end
-    })
-    
-    Tabs.Tab1:AddButton({
-        Title = "KRNL",
-        Description = "Can Run Script But Use Your Executor's Api",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/LongHip2012/Lonely-Hub/refs/heads/main/KRNL%20UI%20Remake.lua.txt"))()
-        end
-    })
-    
-----------------------------------------blox fruit----------------------------------------
-print("blox fruit Tab Loading")
-wait(1)
-Tabs.Tab2:AddButton({
-        Title = "Hiru Hub",
-        Description = "NO KEY",
-        Callback = function()
-            getgenv().Settings = {
-    JoinTeam = true,
-    Team = "Marines"
-}
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kiddohiru/Source/main/BloxFruits.lua"))()
-        end
-    })
-    
-Tabs.Tab2:AddButton({
-        Title = "Quantum Hub",
-        Description = "NO KEY",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/QuantumOnyx.lua"))()
-        end
-    })
-  
-Tabs.Tab2:AddButton({
-        Title = "Blue X Hub",
-        Description = "",
-        Callback = function()
-            getgenv().Config = {
-    ["Misc"] = {
-        ["RandomFruits"] = true,
-        ["BlackScreen"] = false,
-    }
-}
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/refs/heads/main/FindFruits.lua"))()
-        end
-    })
-----------------------------------------grow a gaden----------------------------------------
-print("Loading grow a gaden Tab")
-wait(1)
-Tabs.Tab3:AddParagraph({
-        Title = "grow a gaden tab",
-        Content = "click rồi đợi 5s"
-    })
-    
-Tabs.Tab3:AddButton({
-        Title = "H4x Hub",
-        Description = "NO KEY",
-        Callback = function()
-            loadstring(game:HttpGet('https://raw.githubusercontent.com/H4xScripts/Scripts/refs/heads/main/autofidner', true))()
-        end
-    })
-----------------------------------------steal a branrot----------------------------------------
-print("Loading Script Tab")
-wait(1)
-print("Loading steal brainrot Script")
-wait(.1)
-
-Tabs.Tab4:AddParagraph({
-        Title = "steal brainrot tab",
-        Content = "click rồi đợi 5s"
-    })
-wait(5)
-Tabs.Tab4:AddButton({
-        Title = "Chilli Hub",
-        Description = "NO KEY",  
-        Callback = function()
-           loadstring(game:HttpGet("https://raw.githubusercontent.com/tienkhanh1/spicy/main/Chilli.lua"))() 
-        end
-    })
-    
-    Tabs.BF:AddButton({
-        Title = "Makal Hub",
-        Description = "NO KEY",  
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/DoliScriptz/loader/refs/heads/main/main.lua",true))()
-        end
-    })
-    
- 
-----------------------------------------99 night----------------------------------------
-print("Loading 99 night Tab")
-wait(1)
-Tabs.Blr:AddParagraph({
-        Title = "99 night Tab",
-        Content = "click rồi đợi 5s"
-    })
-    
-Tabs.Blr:AddButton({
-        Title = "Voidware Hub",
-        Description = "NO KEY",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VW-Add/main/nightsintheforest.lua", true))()
-        end
-    })
-    
-    Tabs.Blr:AddButton({
-        Title = "Kenniel Hub",
-        Description = "NO KEY",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/Kenniel123/99-Nights-in-the-Forest/refs/heads/main/99%20Nights%20in%20the%20Forest"))()
-        end
-    })
-    
-
-----------------------------------------hunte zombie----------------------------------------
-print("Loading hunte zombie Script")
-wait(.5)
-Tabs.Shrimp:AddParagraph({
-        Title = "hunte zombie tab"
-        Content = "click rồi đợi 5s"
-    })
-Tabs.Shrimp:AddButton({
-        Title = "Combo Wick Hub",
-        Description = "NO KEY",
-        Callback = function()
-            loadstring(game:HttpGet("https://v0-supabase-secure-storage.vercel.app/api/script/2ca1518801a95abf0dceb898774fa182"))()
-        end
-    })
-    
-    Tabs.Shrimp:AddButton({
-        Title = "Tora Hub",
-        Description = "NO KEY",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/gumanba/Scripts/main/HuntyZombie"))()
-        end
-    })
-----------------------------------------settings----------------------------------------
-print("Load setting Script")
-wait(.5)
-Tabs.AD:AddParagraph({
-        Title = "settings tab",
-        Content = "click rồi đợi 5s"
-    })
-    
-Tabs.AD:AddButton({
-        Title = "Soul Hub",
-        Description = "key",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/GoblinKun009/Script/refs/heads/main/soulhub", true))()
-        end
-    })
-    
-    Tabs.AD:AddButton({
-        Title = "TxZ Hub",
-        Description = "key",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/DrTxZ/Mercure-Hub/refs/heads/main/Mercure%20Hub.lua"))()
-        end
-    })
-----------------------------------------info----------------------------------------
-print("Loading Server Tab")
-wait(.5)
-print("Loading Info Tab")
-wait(.1)
-Tabs.If:AddParagraph({
-        Title = "Player",
-        Content = "Thank "..game.Players.LocalPlayer.Name.." for use script!"
-    })
-
-    Tabs.If:AddParagraph({
-        Title = "những người đã đóng góp script",
-        Content = "not"
-    })
-    
-    Tabs.If:AddParagraph({
-        Title = "Deverloper",
-        Content = "HieuDRG"
-    })
-    
-    Tabs.If:AddButton({
-        Title = "TIKTOK",
-        Description = "INFO",
-        Callback = function()
-            setclipboard("NOT")
-        end
-    })
-        
-    Tabs.If:AddButton({
-        Title = "YOUTOBE",
-        Description = "INFO",
-        Callback = function()
-            setclipboard("NOT")
-        end
-    })
- 
-    wait(.1)
-
-----------------------------------------LoadedNotify----------------------------------------
-    game.StarterGui:SetCore("SendNotification", {
-     Title = "HieuDRG Hub";
-     Text = "Loaded!";
-     Icon = "";
-     Duration = "2";
+    Tabs.Main0:AddButton({
+    Title = "Discord",
+    Description = "TBoyRoblox Community",
+    Callback = function()
+        setclipboard("https://discord.gg/tboyroblox-community-1253927333920899153")
+    end
 })
-wait(.1) 
-print("Loaded! thành công")
-wait(.1)
-warn("script tổng hợp mới nhất")
+
+    Tabs.Main0:AddButton({
+    Title = "Youtuber",
+    Description = "TBoy Roblox",
+    Callback = function()
+        setclipboard("https://www.youtube.com/@TBoyRoblox08")
+    end
+})
+
+    Tabs.Main0:AddButton({
+    Title = "Youtuber",
+    Description = "TBoy Gamer",
+    Callback = function()
+        setclipboard("https://www.youtube.com/@TBoyGamer08")
+    end
+})
+    
+    Tabs.Main1:AddButton({
+    Title="Redz Hub",
+    Description="",
+    Callback=function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/newredz/BloxFruits/refs/heads/main/Source.luau"))(Settings)
+  end
+})
+
+Tabs.Main1:AddButton({
+    Title="GreenZ Hub",
+    Description="",
+    Callback=function()
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaAnarchist/GreenZ-Hub/refs/heads/main/KaitunDoughKing.lua"))()
+  end
+})
